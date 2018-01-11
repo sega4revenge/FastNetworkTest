ed6e15d8c489a0760e5377150a3d71f94831a001
Hoang Linh <hohoanglinh190295@gmail.com>
2018-01-11 13:44:45 +0700

    Boss Linh Commit !

--- app/src/main/java/finger/thuetot/vn/fragment/HomeFragment.kt
+++ app/src/main/java/finger/thuetot/vn/fragment/HomeFragment.kt
@@ -57,6 +57,8 @@ class HomeFragment : Fragment(), ProductAdapter.OnproductClickListener, ProductL
     }
 
     override fun setErrorPhonenumber(errorMessage: String) {
+        Log.e("Cancel button","Cancel buttonnnnnnnnnnnn")
+        accept?.isEnabled  = true
 //        val inflater = layoutInflater
 //        val v = inflater.inflate(R.layout.dialog_phonenumber, null)
         v!!.progressBar_phonenumber.visibility = View.GONE
@@ -92,6 +94,7 @@ class HomeFragment : Fragment(), ProductAdapter.OnproductClickListener, ProductL
     var mAleftdialog: AlertDialog? = null
     var v: View? = null
     var dialog: AlertDialog.Builder? = null
+    var accept : Button ?= null
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -191,14 +194,16 @@ class HomeFragment : Fragment(), ProductAdapter.OnproductClickListener, ProductL
             v = inflater.inflate(R.layout.dialog_phonenumber, null)
             val phonenumber = v!!.findViewById<EditText>(R.id.edt_phonenumber)
             val progressBar = v!!.findViewById<ProgressBar>(R.id.progressBar_phonenumber)
-            val accept = v!!.findViewById<Button>(R.id.btn_accept_phonenumber)
+            accept = v!!.findViewById<Button>(R.id.btn_accept_phonenumber)
             val cancel = v!!.findViewById<Button>(R.id.btn_cancel_phonenumber)
             dialog!!.setView(v)
             dialog!!.setCancelable(false)
             mAleftdialog = dialog!!.show()
 
-            accept.setOnClickListener {
-                CircularAnim.hide(accept)
+            accept?.setOnClickListener {
+                Log.e("Click button","Click buttonnnnnnnnnnnn")
+                accept?.isEnabled = false
+                CircularAnim.hide(accept!!)
                         .endRadius((progressBar.height / 2).toFloat())
                         .go(object : CircularAnim.OnAnimationEndListener {
                             override fun onAnimationEnd() {
@@ -225,7 +230,7 @@ class HomeFragment : Fragment(), ProductAdapter.OnproductClickListener, ProductL
 
                                 } else {
                                     progressBar.visibility = View.GONE
-                                    CircularAnim.show(accept).go()
+                                    CircularAnim.show(accept!!).go()
 //                                    showSnackBarMessage("Enter Valid Details !")
                                 }                            }
                         })
